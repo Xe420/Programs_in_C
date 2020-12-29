c0d81bc8165572813eaaa82639c12068ade38ffc
Elivelton Moraes <64163098+Xe420@users.noreply.github.com>
2020-12-29 18:03:02 -0300

    Add files via upload

---
+++ programs em C/fantastico.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ Este txt é só um comentario.
+  Comentarios são ignorados pelo compilador e são utilizados
+   p/ melhor compreensao do program.
+  */
+
+  int main ()
+  {
+      printf("Como estou me divertindo!!! \n");
+      printf ("Como é bom estudar !!! \n");
+
+      return 0;
+  }
